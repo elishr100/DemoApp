b5ca458bf22e1ff7adc77ebfabecf24243cf80bb
Eli Shriki <Eli.Shriki@nice.com>
2022-03-30 14:55:43 +0300

    remove test

--- src/test/java/com/demo/demoApp/DemoApplicationTests.java
+++ src/test/java/com/demo/demoApp/DemoApplicationTests.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @WebMvcTest(DemoApplicationTests.class)
@@ -17,9 +15,7 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() throws Exception {
 		
-		mockMvc.perform(MockMvcRequestBuilders
-		.get("/"))
-		.andExpect(MockMvcResultMatchers.status().isOk());
+
 	}
 
 }
