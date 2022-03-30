6a2275cc1c4f31a6069ea3e5d47aa787056f439b
Eli Shriki <Eli.Shriki@nice.com>
2022-03-30 15:00:09 +0300

    remove test

--- src/test/java/com/demo/demoApp/DemoApplicationTests.java
+++ src/test/java/com/demo/demoApp/DemoApplicationTests.java
@@ -1,17 +1,12 @@
 package com.demo.demoApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@WebMvcTest(DemoApplicationTests.class)
 class DemoApplicationTests {
-	@Autowired
-	private MockMvc mockMvc;
 
+	
 	@Test
 	void contextLoads() throws Exception {
 		
