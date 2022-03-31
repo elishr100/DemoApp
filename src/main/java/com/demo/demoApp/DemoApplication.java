35381d4063208be4065858d9b05cf39eb37efca3
Eli Shriki <Eli.Shriki@nice.com>
2022-03-31 18:53:49 +0300

    change git_update.sh

--- src/main/java/com/demo/demoApp/DemoApplication.java
+++ src/main/java/com/demo/demoApp/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	@ResponseStatus(value = HttpStatus.OK)
     public String index() {
 		System.out.println("running demoApp application...");
-        return "Hello from Demo application 30";
+        return "Hello from Demo application 40";
     }
 
 	public static void main(String[] args) {
