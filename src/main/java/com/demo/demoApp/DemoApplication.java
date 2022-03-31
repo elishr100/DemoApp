c9aebfe8becdc5f8319a13bed1397337d3e7b1df
Eli Shriki <Eli.Shriki@nice.com>
2022-03-31 15:59:33 +0300

    deploy docket to ECS

--- src/main/java/com/demo/demoApp/DemoApplication.java
+++ src/main/java/com/demo/demoApp/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	@ResponseStatus(value = HttpStatus.OK)
     public String index() {
 		System.out.println("running demoApp application...");
-        return "Hello from Demo application after update";
+        return "Hello from Demo application 2";
     }
 
 	public static void main(String[] args) {
