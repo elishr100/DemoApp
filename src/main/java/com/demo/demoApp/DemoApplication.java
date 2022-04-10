03cae5ae9c37dcb68ca23fff3fd876e126bfb2e9
Eli Shriki <Eli.Shriki@nice.com>
2022-04-10 11:09:23 +0300

    update string

--- src/main/java/com/demo/demoApp/DemoApplication.java
+++ src/main/java/com/demo/demoApp/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	@ResponseStatus(value = HttpStatus.OK)
     public String index() {
 		System.out.println("running demoApp application...");
-        return "Hello from Demo application To DevOps Team";
+        return "Hello from Demo application";
     }
 
 	public static void main(String[] args) {
