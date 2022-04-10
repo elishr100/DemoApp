54df2f821cd8fab70da53629d647f778305d1599
Eli Shriki <Eli.Shriki@nice.com>
2022-04-10 11:42:29 +0300

    change test

--- src/test/java/com/demo/demoApp/DemoApplicationTests.java
+++ src/test/java/com/demo/demoApp/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.demo.demoApp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,8 +10,9 @@ class DemoApplicationTests {
 
 	
 	@Test
-	void contextLoads() throws Exception {
-		
+	void testString() {
+		DemoApplication demoApp = new DemoApplication();
+		assertEquals("Hello from Demo application 2", demoApp.index());
 
 	}
 
