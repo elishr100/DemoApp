2daa4efb55e500de7486f12a103ada99f9070f6c
elishr100 <eli.shriki@nice.com>
2022-12-12 14:44:49 +0200

    update controller

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -3,16 +3,18 @@ package com.demo.demoApp.controllers;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
 
-    //@Autowired
-    //private RestTemplate rest;
+    @Autowired
+    private RestTemplate rest;
 	
 	@GetMapping("/app1")
 	@ResponseStatus(value = HttpStatus.OK)
@@ -25,8 +27,8 @@ public class DemoController {
 			e.printStackTrace();
 		}
 		
-		//String app2_msg = rest.getForObject("http://app2.eli-demo.com/app2", String.class);
-        String app2_msg = "test";
+		String app2_msg = rest.getForObject("http://app2.eli-demo.com/app2", String.class);
+        //String app2_msg = "test";
 
         return message + "       \n" + app2_msg;
     }
