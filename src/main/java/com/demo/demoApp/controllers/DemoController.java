34349c3b67e115f90c5d9d3e1813de3c4dc2b546
elishr100 <eli.shriki@nice.com>
2022-12-12 11:53:07 +0200

    comment restTamplate

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -3,18 +3,16 @@ package com.demo.demoApp.controllers;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
 
-    @Autowired
-    private RestTemplate rest;
+    //@Autowired
+    //private RestTemplate rest;
 	
 	@GetMapping("/app1")
 	@ResponseStatus(value = HttpStatus.OK)
@@ -27,7 +25,8 @@ public class DemoController {
 			e.printStackTrace();
 		}
 		
-		String app2_msg = rest.getForObject("http://app2.eli-demo.com/app2", String.class);
+		//String app2_msg = rest.getForObject("http://app2.eli-demo.com/app2", String.class);
+        String app2_msg = "test";
 
         return message + "       \n" + app2_msg;
     }
