55a55ce7e3034637d532783db20d97749a264408
elishr100 <eli.shriki@nice.com>
2023-01-01 20:18:31 +0200

    discover ip and port

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -3,8 +3,8 @@ package com.demo.demoApp.controllers;
 import java.net.InetAddress;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.services.servicediscovery.AWSServiceDiscovery;
 import com.amazonaws.services.servicediscovery.AWSServiceDiscoveryClientBuilder;
+import com.amazonaws.services.servicediscovery.model.DiscoverInstancesRequest;
+import com.amazonaws.services.servicediscovery.model.DiscoverInstancesResult;
+import com.amazonaws.services.servicediscovery.model.HealthStatus;
+import com.amazonaws.services.servicediscovery.model.HttpInstanceSummary;
+
 
 @RestController
 public class DemoController {
 
+    private static final String AWS_INSTANCE_IPV_4_ATTRIBUTE = "AWS_INSTANCE_IPV4";
+    private static final String AWS_INSTANCE_PORT_ATTRIBUTE = "AWS_INSTANCE_PORT";
+    
     // @Autowired
     // private AWSServiceDiscovery client;
     //private RestTemplate rest;
@@ -23,8 +31,21 @@ public class DemoController {
 	@GetMapping("/app1")
 	@ResponseStatus(value = HttpStatus.OK)
     public String index() throws URISyntaxException {
+
+
 		String message = "Hello From APP1 !!! ";
         final AWSServiceDiscovery awsServiceDiscovery = AWSServiceDiscoveryClientBuilder.defaultClient();
+        final DiscoverInstancesRequest discoverInstancesRequest = new DiscoverInstancesRequest();
+
+        discoverInstancesRequest.setNamespaceName("autoSummary");
+        discoverInstancesRequest.setServiceName("app3");
+        discoverInstancesRequest.setHealthStatus(HealthStatus.HEALTHY.name());
+
+        DiscoverInstancesResult discoverInstancesResult = awsServiceDiscovery.discoverInstances(discoverInstancesRequest);
+        List<HttpInstanceSummary> allInstances = discoverInstancesResult.getInstances();
+        
+        HttpInstanceSummary result = allInstances.get(0);
+        String serviceLocation = result.getAttributes().get(AWS_INSTANCE_IPV_4_ATTRIBUTE) + ":" + result.getAttributes().get(AWS_INSTANCE_PORT_ATTRIBUTE);
 
 		try {
 			InetAddress ip = InetAddress.getLocalHost();
@@ -43,7 +64,7 @@ public class DemoController {
 		//String app2_msg = rest.getForObject(uri, String.class);
         
 
-        return message + "                                             >>>>     " ;
+        return message + "                                             >>>>     " + serviceLocation;
     }
 
     @GetMapping("/")
