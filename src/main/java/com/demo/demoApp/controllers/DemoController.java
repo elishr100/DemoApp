5edaefab3c5806a1006c2c331eaf5eb05155ca81
elishr100 <eli.shriki@nice.com>
2023-01-01 16:49:51 +0200

    get cred

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
 import com.amazonaws.services.servicediscovery.AWSServiceDiscovery;
 import com.amazonaws.services.servicediscovery.AWSServiceDiscoveryClientBuilder;
@@ -32,11 +33,13 @@ public class DemoController {
 		}
 	
        
-        AWSServiceDiscovery client = AWSServiceDiscoveryClientBuilder
-            .standard()
-            .withRegion(System.getenv("us-east-1"))
-            .withCredentials(new EC2ContainerCredentialsProviderWrapper())
-            .build();
+        AWSCredentials cred = new EC2ContainerCredentialsProviderWrapper().getCredentials();
+        
+        // AWSServiceDiscovery client = AWSServiceDiscoveryClientBuilder
+        //     .standard()
+        //     .withRegion(System.getenv("us-east-1"))
+        //     .withCredentials(new EC2ContainerCredentialsProviderWrapper())
+        //     .build();
         
 
         // DiscoverInstancesRequest request = new DiscoverInstancesRequest();
