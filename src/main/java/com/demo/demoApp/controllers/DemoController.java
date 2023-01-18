b416f78acf4c4cb354d2cfdbd0233eb12aa95328
elishr100 <eli.shriki@nice.com>
2023-01-18 12:31:44 +0200

    use RestTemplate

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -2,23 +2,24 @@ package com.demo.demoApp.controllers;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.demo.demoApp.discovery.common.service.ServiceDiscoveryServiceImpl;
-import com.demo.demoApp.discovery.common.util.ServiceDiscoveryConstants;
+//import com.demo.demoApp.discovery.common.service.ServiceDiscoveryServiceImpl;
+//import com.demo.demoApp.discovery.common.util.ServiceDiscoveryConstants;
 
 @RestController
 public class DemoController {
 
     @Autowired
-    private ServiceDiscoveryServiceImpl serviceDiscoveryService;
-    //private RestTemplate rest;
+    //private ServiceDiscoveryServiceImpl serviceDiscoveryService;
+    private RestTemplate rest;
 	
 	@GetMapping("/app1")
 	@ResponseStatus(value = HttpStatus.OK)
@@ -34,11 +35,11 @@ public class DemoController {
 			e.printStackTrace();
 		}
 	     
-        String hostIp = serviceDiscoveryService.getServiceLocationResolver().resolve(ServiceDiscoveryConstants.SERVICE_B);
-		//String app2_msg = rest.getForObject(uri, String.class);
+        //String hostIp = serviceDiscoveryService.getServiceLocationResolver().resolve(ServiceDiscoveryConstants.SERVICE_B);
+		String app2_msg = rest.getForObject("http://app2.autosummary", String.class);
         
 
-        return message + "                                             >>>>     " +  hostIp;
+        return message + "                                             >>>>     " +  app2_msg;
     }
 
     @GetMapping("/")
