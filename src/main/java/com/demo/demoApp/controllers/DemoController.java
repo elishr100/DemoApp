06621f1ddfe7dc255f10073e6dafd8afde646a29
elishr100 <eli.shriki@nice.com>
2023-06-12 19:05:22 +0300

    add health check

--- src/main/java/com/demo/demoApp/controllers/DemoController.java
+++ src/main/java/com/demo/demoApp/controllers/DemoController.java
@@ -6,13 +6,12 @@ import java.net.UnknownHostException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 //import com.demo.demoApp.discovery.common.service.ServiceDiscoveryServiceImpl;
@@ -26,6 +25,15 @@ public class DemoController {
     private RestTemplate rest;
 	private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 	
+
+	@GetMapping("/actuator/health")
+	public ResponseEntity<String> healthCheck() {
+	  // Perform any necessary health checks here
+	  
+		return ResponseEntity.ok("App1 is healthy");
+
+	}
+
 	@GetMapping("/app1")
 	@ResponseStatus(value = HttpStatus.OK)
     public String index() {
